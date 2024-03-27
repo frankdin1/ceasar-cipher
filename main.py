f2c7f1ac2c6a03fde7eeebb1ea5080233c84cab4
frankdin <frankdin@yahoo.com>
2024-03-27 17:06:00 -0400

    Replaced individual encrypt and decrypt functions with single function that performs encryption and decryption based on user input.

--- main.py
+++ main.py
@@ -6,37 +6,22 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(user_text, user_shift):
+def shift_text(user_text, user_shift):
+    shifted_index = 0
     encrypted_text = ""
     for char in user_text:
         if char == " ":
             encrypted_text += " "
             continue
         char_index = alphabet.index(char)  # the index of the character in the alphabet array
-        shifted_index = char_index + user_shift  # the new index after applying the cypher
+        if direction == "encode":
+            shifted_index = char_index + user_shift
+        elif direction == "decode":
+            shifted_index = char_index - user_shift
         if shifted_index > 26:
             shifted_index %= 26
         encrypted_text += alphabet[shifted_index]
     return encrypted_text
 
 
-def decrypt(user_text, user_shift):
-    decrypted_text = ""
-    for char in user_text:
-        if char == " ":
-            decrypted_text += " "
-            continue
-        char_index = alphabet.index(char)  # the index of the character in the alphabet array
-        shifted_index = char_index - user_shift  # the new index after applying the cypher
-        if shifted_index > 26:
-            shifted_index %= 26
-        decrypted_text += alphabet[shifted_index]
-    return decrypted_text
-
-
-if direction == "encode":
-    encoded_text = (encrypt(text, shift))
-    print(encoded_text)
-else:
-    decoded_text = decrypt(text, shift)
-    print(decoded_text)
+print(shift_text(text, shift))
